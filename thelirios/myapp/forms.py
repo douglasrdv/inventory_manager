fdad35cdd77ae2a5d91bab8ccd9b576afe1191d7
eric <ericwduarte@gmail.com>
2024-04-28 03:11:09 -0300

    feat: Added ingregient update and delete, added icon with links

--- thelirios/myapp/forms.py
+++ thelirios/myapp/forms.py
@@ -10,10 +10,23 @@ from .models import Ingredient, Recipe
 class IngredientForm(forms.ModelForm):
     class Meta:
         model = Ingredient
+        print(model)
         fields = ["name", "weight", "cost"]
 
 
 class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [
+            "title",
+            "ingredients",
+            "amount_yield",
+            "cooking_time",
+            "description",
+            "cost",
+        ]
+
+class RecipeUpdateForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = [
--- thelirios/myapp/urls.py
+++ thelirios/myapp/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("new-ingredient", views.ingredient_registration, name="new-ingredient"),
     path("ingredients-list", views.list_all_ingredients, name="ingredients-list"),
+    path("ingredient-delete/<int:id>", views.ingredient_delete, name="ingredient-delete"),
+    path("ingredient-update/<int:id>", views.ingredient_update, name="ingredient-update"),
     path("new-recipe", views.recipe_registration, name="new-recipe"),
     path("recipes-list", views.list_all_recipes, name="recipes-list"),
 ]
