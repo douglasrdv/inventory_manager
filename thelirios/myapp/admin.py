b1d8ac5996b9fc667df846e7be4789b431d147ee
Douglas <douglasrdv@proton.me>
2024-05-21 20:28:16 -0300

    style: Change imports order

--- thelirios/myapp/admin.py
+++ thelirios/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, Product, IngredientInventory, RecipeInventory, IngredientToInventory, RecipeToInventory, ProductInventory, ProductToInventory
+from .models import Ingredient, Recipe, Product, IngredientInventory, IngredientToInventory, RecipeInventory, RecipeToInventory, ProductInventory, ProductToInventory
 
 
 admin.site.register(Ingredient)
