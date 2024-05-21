a80be356184951b00860a6a3ffd7cfbceca103f1
Douglas <douglasrdv@proton.me>
2024-05-21 13:39:44 -0300

    fix: Add route to urls

--- thelirios/project/urls.py
+++ thelirios/project/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     path('accounts/', include('allauth.urls')),
-    path('accounts/profile/', views.profile, name='profile')
+    path('accounts/profile/', views.profile, name='profile'),
 ]
